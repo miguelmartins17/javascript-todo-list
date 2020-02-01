d6f6e6a45a3001caccfc473424e9b4d79de9f545
MiguelGcodes <miguelguilherme2001@gmail.com>
2020-02-01 13:37:32 +0000

    Setup code to execute the functions
    Setup code that requires the libraries/files so we can execute the functions

--- test/todo-app.test.js
+++ test/todo-app.test.js
@@ -1,1 +1,7 @@
-
+const test = require ('tape'); //https://github.com/dwyl/learn-tape
+const fs = require ('fs');  //to read html files (see below)
+const path = require ('path');  // so we can open files cross-platform
+const html = fs.readFileSync(path.resolve(__dirname, '../index.html'));
+require('jsdom-global')(html);  // https://github.com/rstacruz/jsdom-global
+const app = require('../lib/todo-app.js'); // functions to test
+const id = 'test-app';      // all tests use 'test-app' as root element 
